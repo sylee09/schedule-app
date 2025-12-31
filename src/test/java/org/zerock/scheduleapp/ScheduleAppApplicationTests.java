4ae7c50cc3340f09ea53751b545e1fefbb81c712
Seungyoung <leesy960909@gmail.com>
2025-12-31 10:53:24 +0900

    first commit

---
+++ src/test/java/org/zerock/scheduleapp/ScheduleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.scheduleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
