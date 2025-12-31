a76fa72c1d39c7692de186eb966a9f660d482094
Seungyoung <leesy960909@gmail.com>
2025-12-31 16:16:16 +0900

    refactor : 테스트 코드 리펙토링
    @BeforeEach 사용하여 세팅 단순화함

--- src/test/java/org/zerock/scheduleapp/service/ScheduleServiceTest.java
+++ src/test/java/org/zerock/scheduleapp/service/ScheduleServiceTest.java
@@ -3,6 +3,7 @@ package org.zerock.scheduleapp.service;
 import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,36 +29,39 @@ class ScheduleServiceTest {
     @Autowired
     EntityManager em;
 
+    private ScheduleResponseDTO scheduleResponseDTO1;
+    private ScheduleResponseDTO scheduleResponseDTO2;
+    private ScheduleResponseDTO scheduleResponseDTO3;
+
+    @BeforeEach
+    public void init() {
+        ScheduleRequestDTO scheduleRequestDTO1 = new ScheduleRequestDTO();
+        scheduleRequestDTO1.setAuthor("lee");
+        scheduleRequestDTO1.setContent("테스트1");
+        scheduleResponseDTO1 = scheduleService.addSchedule(scheduleRequestDTO1);
+
+        ScheduleRequestDTO scheduleRequestDTO2 = new ScheduleRequestDTO();
+        scheduleRequestDTO2.setAuthor("lee");
+        scheduleRequestDTO2.setContent("테스트2");
+        scheduleResponseDTO2 = scheduleService.addSchedule(scheduleRequestDTO2);
+
+        ScheduleRequestDTO scheduleRequestDTO3 = new ScheduleRequestDTO();
+        scheduleRequestDTO3.setAuthor("lee");
+        scheduleRequestDTO3.setContent("테스트3");
+        scheduleResponseDTO3 = scheduleService.addSchedule(scheduleRequestDTO3);
+    }
+
     @DisplayName("스케줄 추가 함수 테스트")
     @Test
     public void saveSchedule() {
-        ScheduleRequestDTO scheduleRequestDTO = new ScheduleRequestDTO();
-        scheduleRequestDTO.setAuthor("lee");
-        ScheduleResponseDTO scheduleResponseDTO = scheduleService.addSchedule(scheduleRequestDTO);
-
-        assertThat(scheduleResponseDTO.getAuthor()).isEqualTo("lee");
-        assertThat(scheduleResponseDTO.getCreatedAt()).isNotNull();
-        assertThat(scheduleResponseDTO.getLastUpdatedAt()).isNotNull();
+        assertThat(scheduleResponseDTO1.getAuthor()).isEqualTo("lee");
+        assertThat(scheduleResponseDTO1.getCreatedAt()).isNotNull();
+        assertThat(scheduleResponseDTO1.getLastUpdatedAt()).isNotNull();
     }
 
     @DisplayName("작성자명 기준 전체 일정 조회")
     @Test
     public void findScheduleByAuthor() {
-        ScheduleRequestDTO scheduleRequestDTO1 = new ScheduleRequestDTO();
-        scheduleRequestDTO1.setAuthor("lee");
-        scheduleRequestDTO1.setContent("테스트1");
-        scheduleService.addSchedule(scheduleRequestDTO1);
-
-        ScheduleRequestDTO scheduleRequestDTO2 = new ScheduleRequestDTO();
-        scheduleRequestDTO2.setAuthor("lee");
-        scheduleRequestDTO2.setContent("테스트2");
-        scheduleService.addSchedule(scheduleRequestDTO2);
-
-        ScheduleRequestDTO scheduleRequestDTO3 = new ScheduleRequestDTO();
-        scheduleRequestDTO3.setAuthor("lee");
-        scheduleRequestDTO3.setContent("테스트3");
-        scheduleService.addSchedule(scheduleRequestDTO3);
-
         List<ScheduleResponseDTO> lee = scheduleService.findAllSchedulesByAuthor("lee");
 
         assertThat(lee.size()).isEqualTo(3);
@@ -68,26 +72,17 @@ class ScheduleServiceTest {
     @DisplayName("스케줄 id 기준 조회")
     @Test
     public void findScheduleById() {
-        ScheduleRequestDTO scheduleRequestDTO1 = new ScheduleRequestDTO();
-        scheduleRequestDTO1.setAuthor("lee");
-        scheduleRequestDTO1.setContent("테스트1");
-        ScheduleResponseDTO scheduleResponseDTO = scheduleService.addSchedule(scheduleRequestDTO1);
+        ScheduleResponseDTO found = scheduleService.viewScheduleById(scheduleResponseDTO1.getId());
 
-        ScheduleResponseDTO found = scheduleService.viewScheduleById(scheduleResponseDTO.getId());
+        assertThat(found).isEqualTo(scheduleResponseDTO1);
 
-        assertThat(found).isEqualTo(scheduleResponseDTO);
-
-        assertThatThrownBy(() -> scheduleService.viewScheduleById(10L)).isExactlyInstanceOf(NotExistException.class);
+        assertThatThrownBy(() -> scheduleService.viewScheduleById(1000L)).isExactlyInstanceOf(NotExistException.class);
     }
 
     @DisplayName("스케줄 수정 메서드 테스트")
     @Test
     public void updateSchedule() throws InterruptedException {
-        ScheduleRequestDTO scheduleRequestDTO1 = new ScheduleRequestDTO();
-        scheduleRequestDTO1.setAuthor("lee");
-        scheduleRequestDTO1.setContent("테스트1");
-        ScheduleResponseDTO scheduleResponseDTO = scheduleService.addSchedule(scheduleRequestDTO1);
-        LocalDateTime lastUpdated = scheduleResponseDTO.getLastUpdatedAt();
+        LocalDateTime lastUpdated = scheduleResponseDTO1.getLastUpdatedAt();
 
         Thread.sleep(10);
 
@@ -95,7 +90,7 @@ class ScheduleServiceTest {
         scheduleUpdateDTO.setAuthor("park");
         scheduleUpdateDTO.setTitle("update");
 
-        ScheduleResponseDTO updatedSchedule = scheduleService.updateSchedule(scheduleResponseDTO.getId(), scheduleUpdateDTO);
+        ScheduleResponseDTO updatedSchedule = scheduleService.updateSchedule(scheduleResponseDTO1.getId(), scheduleUpdateDTO);
 
         assertThat(updatedSchedule.getAuthor()).isEqualTo("park");
         assertThat(updatedSchedule.getTitle()).isEqualTo("update");
